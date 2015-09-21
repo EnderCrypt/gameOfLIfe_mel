338c7405625aad53458343d5a7738e9748cf86a7
Magnu <EnderCrypt@gmail.com>
2015-09-21 20:31:12 +0200

    added screensize variable

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
+import java.awt.Dimension;
 
 public class Main
 	{
-
+	public static Dimension screenSize = new Dimension(100,30);
 	public static void main(String[] args)
 		{
-		// TODO Auto-generated method stub
-
+		if (args.length >= 2)
+			{
+			screenSize.width = Integer.parseInt(args[0]);
+			screenSize.height = Integer.parseInt(args[1]);
+			}
 		}
-
 	}
