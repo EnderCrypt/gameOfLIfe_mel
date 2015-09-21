a1963c66b85a2db3e206a7d69db8e322545e99a0
Magnu <EnderCrypt@gmail.com>
2015-09-21 22:18:14 +0200

    fixed resoultion and population bug(?)

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.awt.Dimension;
 
 public class Main
 	{
-	public static Dimension screenSize = new Dimension(100,30);
+	public static Dimension screenSize = new Dimension(53,180);
 	public static void main(String[] args)
 		{
 		// gets the screensize as parameter
@@ -20,6 +20,7 @@ public class Main
 			Simulation.populate((int)((screenSize.width*screenSize.height)*0.2));
 			boolean[][] life = Simulation.update();
 			//Printer
+			System.out.println("---------");
 			Printer.printBool(life);
 			//sleep
 			try{Thread.sleep(1000);}catch(InterruptedException e){e.printStackTrace();}
