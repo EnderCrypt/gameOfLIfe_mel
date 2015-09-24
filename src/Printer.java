a398250b962a373873461f6be0f13ecca766a7fa
Melik Kaya <melik_kaya16@hotmail.com>
2015-09-24 13:44:57 +0200

    made the outputs return as Strings

--- src/Printer.java
+++ src/Printer.java
@@ -2,7 +2,7 @@ import java.awt.Dimension;
 
 public class Printer
 {
-	public static boolean[][] outputArray;
+	//public static boolean[][] outputArray;
 	public static Dimension screenSize;
 	
 	
@@ -13,17 +13,21 @@ public class Printer
 
 	public static void printBool(boolean [][] life)
 	{
+		
 		for(int y=0;y<screenSize.height-1;y++)
 		{
+			String output="";
 			for(int x=0;x<screenSize.width-1;x++)
 			{
 				if(life[x][y])
-					System.out.print('@');
+					output=output+"@";
+				
 				else
-					System.out.print('.');
-					//put(point.x, point.)
-			}
-			System.out.println();
+					output=output+".";
+			}			
+			System.out.println(output); //output=output+"\n";
 		}
+		
+		
 	}
 }//class
